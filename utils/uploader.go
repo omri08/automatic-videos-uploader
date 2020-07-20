1ffdde1904a23a2522acb9b98a609fea61f609db
omri08 <54754198+omri08@users.noreply.github.com>
2020-07-20 13:32:59 +0300

    Created youtube uploader

---
+++ utils/uploader.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"google.golang.org/api/youtube/v3"
+)
+
+// Uploader uploads the file to youtube
+func main() {
+	flag.Parse()
+
+	if *filename == "" {
+		log.Fatalf("You must provide a filename of a video file to upload")
+	}
+
+	client := getClient(youtube.YoutubeUploadScope)
+
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Error creating YouTube client: %v", err)
+	}
+
+	upload := &youtube.Video{
+		Snippet: &youtube.VideoSnippet{
+			Title:       *title,
+			Description: *description,
+			CategoryId:  *category,
+		},
+		Status: &youtube.VideoStatus{PrivacyStatus: *privacy},
+	}
+
+	// The API returns a 400 Bad Request response if tags is an empty string.
+	if strings.Trim(*keywords, "") != "" {
+		upload.Snippet.Tags = strings.Split(*keywords, ",")
+	}
+
+	call := service.Videos.Insert("snippet,status", upload)
+
+	file, err := os.Open(*filename)
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("Error opening %v: %v", *filename, err)
+	}
+
+	response, err := call.Media(file).Do()
+	handleError(err, "")
+	fmt.Printf("Upload successful! Video ID: %v\n", response.Id)
+}
